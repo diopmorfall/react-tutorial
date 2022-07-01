0a4a13a0ead6cbdff1995e532c00c8ac321c3c8e
diopmorfall <morfall.papydiop99@gmail.com>
2022-07-01 17:34:30 +0200

    First commit, lists and keys part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,324 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function intro(){
+    return "I'm Gol D. Roger, the king of the pirates";
+}
+//* I can put everything inside the curly braces (kinda interpolation in Angular)
+
+//* after compilation JSX expressions become regular JS functions, so I can use it like this
+
+function greeting(user){
+    if(user){
+        return <h3>Hi {user}, wassup?</h3>;
+    } else {
+        return <h3>Wait a minute, who are you ?</h3>; 
+    }
 }
 
-export default App;
+//* Components and props
+//? Components allow us to split the UI in reusable and isolated parts (React components)
+
+function sayhi(props){
+    return <p style={"background-color: pink"}>Hi {props.name}, I'm a function component because I'm a regular JS function</p>
+}
+
+//* A valid component, because it takes a props object (properties) and returns a React element that describes its appearance
+
+class SayHi extends React.Component {
+    render(){
+        return  <p>
+                    Hi {this.props.name}, I'm a component defined as a class<br/>
+                </p>;
+    }
+}
+
+//* to use the props object and embed components better, this way is better
+//? JSX attributes and children are passed into it 
+
+const sayHi = <SayHi name="Mary" />;
+//! Always write them with the first capital letter
+
+function App() {
+    //* Attributes can be defined as static strings, or embedded in a JSX expression {}
+	return (
+		<div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h2>{intro()}</h2>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React Worororo
+                </a>
+                <div>{greeting("Savage")}</div>
+                <p>
+                    The <code>App</code> component is rendered because it's exported from here, and imported in <code>index.js</code>
+                </p>
+                <h4>Like we see above, a JSX tag can contain children</h4>
+                <h6>Also, React DOM escapes the values in JSX before rendering them, preventing XSS attacks</h6>
+                <h4>Babel compiles JSX with <code>React.createElement()</code> function</h4>
+                <p>
+                    It makes some checks and creates an object representing our elements: this below<br/>
+                    <code>
+                        return (
+                            <button className="mama">Loco</button>
+                        );
+                    </code>
+                    <br/>
+                    is equal to XX in comments: React elements
+                </p>
+                <h5>React elements describe what you want to see: they're read and used to build and update the DOM</h5>
+            </header>
+            <section>
+                <h2>Components and Props</h2>
+                    {sayHi}
+                    <div>
+                        <h3>Multiple components inside one</h3>
+                        <SayHi name="Jane" />
+                        <SayHi name="Fuji" />
+                        <SayHi name="Garp" />
+                    </div>
+                    <h3>If possible, we can split components into smaller ones: this way they're easier to manage</h3>
+                    <p>
+                        This way, one component doesn't have many tasks and doesn't know who's handling it:<br/>
+                        <code>it has to be as generic as possible</code>, making them reusable in other contexts<br/><br/>
+                        Also <code>a component mustn't modify its properties</code>: it must be a <code>pure function</code> 
+                        (or class) that doesn't alter them; it'll be handled with states
+                    </p>
+            </section>
+            <hr/>
+            <section>
+                <h2>State and lifecycle (run the clock)</h2>
+            </section>
+            <hr/>
+            <section>
+                <h2>Handling events</h2>
+                <p>
+                    Note that:
+                    <ul>
+                        <li><code>events are named with camelCase</code>;</li>
+                        <li><code>with JSX, we pass a function instead of a string to handle them</code></li>
+                    </ul>
+                </p>
+                <code></code>
+                <form onSubmit={handleSubmit}>
+                    <p>
+                        You can pass a handler method when the component is rendered, instead of using addEventListener.<br/>
+                    </p>
+                    <input type="text" placeholder="Loco"></input>
+                    <button onClick={fire}>Fire it</button>
+                </form>
+                <Toggle />
+            </section>
+            <hr/>
+            <section>
+                <h2>Conditional rendering</h2>
+                <LoginControl />
+                <Chat msgToRead={msg}/>
+                <WarningContainer />
+            </section>
+            <hr/>
+            <section>
+                <h2>Lists and keys</h2>
+                <p>We can transform arrays in lists of elements, and then render it</p>
+                <NumberList numbers={ onis } />
+            </section>
+            <hr/>
+		</div>
+	);
+}
+
+function fire(){
+    alert("We fired it");
+}
+
+function handleSubmit(event){ //* a SyntheticEvent
+    event.preventDefault();
+    //! this is the only way to prevent an event, return false doesn't work
+    //? but here, the React event object is accessible
+}
+
+class Toggle extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {isToggleOn: true};
+
+        this.handleToggle = this.handleToggle.bind(this);
+        //! binding is needed to make it work: class methods aren't bound by default (it's not called as handleToggle())
+        //? we do all the bindings in the constructor
+
+        //* There are some alternatives to binding, both with arrow functions:
+            //? <form onSubmit={(e) => this.handleSubmit(e)}> / this could impact performances
+                //? with this, I can also pass arguments
+            //? handleToggle = () => {} / this is an experimental way of defining a method
+    }
+
+    handleToggle(){
+        this.setState(previousState => ({
+            isToggleOn: !previousState.isToggleOn
+        }))
+    }
+
+    render(){
+        return(
+            <button onClick={this.handleToggle}>
+                {this.state.isToggleOn ? 'ON' : 'OFF'}
+            </button>
+        );
+    }
+}
+
+//? XX looks like this
+//? const element = {
+//?    type: "h1",
+//?    props: {
+//?        className: 'mama',
+//?        children: 'Loco'
+//?    }
+//?};
+
+//* Conditional rendering
+
+function UserAccess(){
+    return <h4>Yeah man, you're back</h4>
+}
+
+function GuestAccess(){
+    return <h4>Who the heck are you ? Login please</h4>
+}
+
+function Access(props){
+    const isLoggedIn = props.isLoggedIn;
+    if(isLoggedIn){
+        return <UserAccess />;
+    }
+
+    return <GuestAccess />;
+}
+
+function LoginButton(props){
+    return (
+        <button onClick={props.onClick}>Login</button>
+    )
+}
+
+function LogoutButton(props){
+    return (
+        <button onClick={props.onClick}>Logout</button>
+    )
+}
+class LoginControl extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = {isLoggedIn: false};
+    }
+
+    handleLoginClick(){
+        this.setState({isLoggedIn: true});
+    }
+
+    handleLogoutClick(){
+        this.setState({isLoggedIn: false});
+    }
+
+    render(){
+        const isLoggedIn = this.state.isLoggedIn;
+        let button;
+        //* Saving an element in a variable and then decide when to show it without messing up with other components
+        
+        //? this if can be shorten like the alternative below, but it's less obvious
+        /*
+        if(isLoggedIn){
+            button = <LogoutButton onClick={this.handleLogoutClick} /> ;
+        } else {
+            button = <LoginButton onClick={this.handleLoginClick} /> ;
+        }
+        */
+        return (
+            <div>
+                <Access isLoggedIn={isLoggedIn} />
+                {isLoggedIn ? 
+                    <LogoutButton onClick={this.handleLogoutClick} /> :
+                    <LoginButton onClick={this.handleLoginClick} />
+                }
+            </div>
+        )
+
+        //* you can choose what you want to show; better if it's something kinda small
+    }
+}
+
+//*  if is ok, but there are shorter alternatives
+const msg = ["Loco", "Pam"];
+
+function Chat(props){
+    const msgToRead = props.msgToRead;
+    return(
+        <div>
+            <h5>Welcome</h5>
+            {msgToRead.length > 0 &&
+                <p>
+                    You have {msgToRead.length} messages to read
+                </p>
+            }
+        </div>
+    )
+
+    //* here we're embedding expressions in jsx with the && operator: if the first part is true, then the next operator will be shown
+    //* otherwise, it'll return false and show nothing
+}
+
+function WarningBanner(props){
+    if(!props.warn) return null; //* warn
+    //? this prevents the component to not be shown, but the lifecycle methods will still be called (componentDidMount)
+
+    return(
+        <div>
+            <h3>WARNING!!!</h3>
+        </div>
+    )
+}
+
+class WarningContainer extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {showWarning: true};
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        this.setState(state => ({
+            showWarning: !state.showWarning
+        }));
+    }
+
+    render(){
+        return(
+            <div>
+                <WarningBanner warn={this.state.showWarning}/>
+                <button onClick={this.handleClick}>
+                    {this.state.showWarning ? 'Hide' : 'Show'}
+                </button>
+            </div>
+        )
+    }
+}
+
+function NumberList(props) {
+    const numbers = props.numbers;
+    const numbersList = numbers.map(number => 
+        <li><i>{ number.toUpperCase() }</i></li>
+    )
+
+    return(
+        <ul>{ numbersList }</ul>
+    )
+}
+
+const onis = ["Haccha", "Juki", "Fuga", "Jaki", "Inbi"]; 
+export default App; //? a regular export after the definition of our component; it'll be available wherever it'll be imported
--- src/index.js
+++ src/index.js
@@ -5,11 +5,108 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+//* the root element that contains all the elements, then is rendered with its content inside with the following method
+//? makes sense that its in the index file
+
+const app = <React.StrictMode>
+                <App />
+            </React.StrictMode>;
+
+//! elements !== components
+
+function tick() {
+    const element = (
+        <div>
+            <h1>Timer here!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}</h2>
+            <code>
+                Usually, an element that is rendered cannot be changed (this functions is scheduled to render the content each
+                second); however, there are more advanced ways to handle this scenario
+            </code>
+        </div>
+    );
+    //root.render(element);
+
+    //* State and lifecycle (making the clock working)
+    //? a state is needed to make the clock update itself
+    //* it's like props, but it's private and handled by the component itself
+
+    root.render(<Clock />);
+}
+
+root.render( //* this renders elements, that are unchangeable once they're shown
+    app
 );
+//*usually, the root renders an element (<App />) that contains everything, and then handle changes with stateful components
+
+//* React DOM compares each element and its children to the previous one, and it updates the DOM to match the required state
+//?  (check the clock example, the DOM always changes)
+
+//setInterval(tick, 1000);
+//* We can make it better, State and Lifecycle (with a component class)
+class Clock extends React.Component {
+
+    constructor(props){
+        super(props); //* passed to the basic React constructor (always this way)
+        this.state = {time: new Date()};
+        //? this.state = {posts: [], comments: []}; X
+        //* here we're assigning the initial state (current time)
+    }
+    
+    //* we also need lifecycle methods (useful when freeing up resources when components are destroyed)
+
+    componentDidMount(){
+        //* runs after the <Clock /> content is rendered to the DOM => mounting
+        this.timerId = setInterval(() => this.tick(), 1000);
+        //? here we're adding an extra field that won't be used anywhere else
+    }
+
+    componentWillUnmount(){
+        //* runs when <Clock /> is removed from the dom => unmounting
+        clearInterval(this.timerId);
+    }
+
+    tick(){
+        //* at each call, we're assigning a new state: React knows it changed, then calls render()
+        this.setState({time: new Date()});
+        //! either we change a state this way, or we set it in the constructor
+    }
+
+    //? calling setState merges the object you pass into the current state, and then you can set its properties X
+
+    //? this.setState({posts: response.posts}); => only sets the posts, and leaves the comments (we can set them later on)
+    //* the object we pass in setState() replaces the object in this.state.object
+
+    //* Sometimes multiple setState calls will be batched (kinda compressed) into one, to enhance performances
+    //* states and props may be updated asynchronously: in that case, we should use this form of setState
+    
+    withAsyncUpdates(){
+        this.setState((state, props) => ({
+            update: state.state + props.num //* the counter example, but it could be anything
+        }));
+    }
+
+    //* Parent or chinld components can't know it a component is stateful/less:
+    //* state is local/encapsulated, only the component who defines it can see it
+
+    //* But a component can pass its state as a prop to a child element
+    //? an hypothetycal <Date ={this.state.date}> => props.date.dowhatyouwantwithit()
+    //* This is a top-down data flow, props go down through childrens
+
+    //? this will be called at each update, but as we render the component in the same dom node we'll only use a single class
+    //? react learns what will be displayed, and updates the dom
+    render(){
+        return(
+            <div>
+                <h3>Updated clock</h3>
+                <h4>{this.state.time.toLocaleTimeString()}</h4>
+            </div>
+        );
+        //? Previously: <h4>{this.props.time.toLocaleTimeString()}</h4>
+    }
+}
+
+//? root.render(<Clock />); to run the State and Lifecycle section
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
