e136a9b674d4f4da87fa399a511fffb5e01c96a1
diopmorfall <morfall.papydiop99@gmail.com>
2022-08-16 21:27:45 +0200

    Editing of the social links

--- digital-business-card/src/components/Footer.js
+++ digital-business-card/src/components/Footer.js
@@ -1,22 +1,13 @@
 import react from 'react';
 import { ReactDOM } from 'react-dom';
-import Logo from '../logo.svg';
 
 export default function Footer(){
     return(
         <div className='footer'>
-            <a href='https://medium.com/@Mor_Fall_Diop'>
-                <img src={Logo} target="_blank" alt='Medium'/>
-            </a>
-            <a href='https://www.facebook.com/Mor.Fall.Diop.99'>
-                <img src={Logo} target="_blank" alt='Facebook'/>
-            </a>
-            <a href='https://www.instagram.com/papy_diop/'>
-                <img src={Logo} target="_blank" alt='Instagram'/>
-            </a>
-            <a href='https://github.com/diopmorfall'>
-                <img src={Logo} target="_blank" alt='GitHub'/>
-            </a>
+            <a href='https://medium.com/@Mor_Fall_Diop'>Me</a>
+            <a href='https://www.facebook.com/Mor.Fall.Diop.99'>fb</a>
+            <a href='https://www.instagram.com/papy_diop/'>In</a>
+            <a href='https://github.com/diopmorfall'>Git</a>
         </div>
     )
 }
