80f5bebcd4a2d505cdc3c67018a23874a7c29b8b
diopmorfall <morfall.papydiop99@gmail.com>
2022-08-16 17:11:45 +0200

    Basic configuration of the project completed, styles added

--- digital-business-card/src/components/Footer.js
+++ digital-business-card/src/components/Footer.js
@@ -1,20 +1,21 @@
 import react from 'react';
 import { ReactDOM } from 'react-dom';
+import Logo from '../logo.svg';
 
 export default function Footer(){
     return(
         <div className='footer'>
             <a href='https://medium.com/@Mor_Fall_Diop'>
-                <img />
+                <img src={Logo} target="_blank" alt='Medium'/>
             </a>
             <a href='https://www.facebook.com/Mor.Fall.Diop.99'>
-                <img />
+                <img src={Logo} target="_blank" alt='Facebook'/>
             </a>
             <a href='https://www.instagram.com/papy_diop/'>
-                <img />
+                <img src={Logo} target="_blank" alt='Instagram'/>
             </a>
             <a href='https://github.com/diopmorfall'>
-                <img />
+                <img src={Logo} target="_blank" alt='GitHub'/>
             </a>
         </div>
     )
--- digital-business-card/src/components/Intro.js
+++ digital-business-card/src/components/Intro.js
@@ -1,12 +1,13 @@
 import react from 'react';
 import { ReactDOM } from 'react-dom';
 import SocialButtons from './SocialButtons';
+import Image from '../profile-pic.png';
 
 
 export default function Intro(){
     return( 
         <header className='header'>
-            <img className='profile-pic' alt='Mor Fall Diop'/>
+            <img className='profile-pic' src={Image} alt='Mor Fall Diop'/>
             <h2>Mor Fall Diop</h2>
             <p>Frontend Developer</p>
             <a href='https://diopmorfall.github.io/portfolio'>Mor's website</a>
